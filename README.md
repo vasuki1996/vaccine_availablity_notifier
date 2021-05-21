98bcb3ee1aa5e6dddf01d997c4c398bea1e27f7f
Vasuki Vardhan G <vasukiv@geekyants.com>
2021-05-21 16:18:52 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# vaccine_availablity_notifier
-Vaccine Availablity Notifier
+# Vaccine Availablity Notifier
+
+This a simple lambda function that checks for vaccine availabilty by district for 7 days and upon getting availability it'll notify you over slack
+
+Configurations
+
+```json
+{
+    "NODE_ENV": "prod",
+    "SLACK_HOOK_URL": "<your_slack_webhook_url_here>",
+    "DISTRICT_ID": "000"
+}
+```
+
+**NOTE => Goto cowin.gov.in without logging in just do a search by district and inspect the network tab for the district id**
+
+
+* Deploy the service
+
+```bash
+npm run deploy
+```
+
+* You can also set a scheduled job on cloudwatch to check availabilty periodically
+* Also make sure you deploy in **"ap-south-1"** Mumbai region as there is a geo restriction
---
+++ index.js
@@ -0,0 +1,86 @@
+const fetch = require('node-fetch');
+const { IncomingWebhook } = require('@slack/webhook');
+
+module.exports.handler = (payload, context, callback) => {
+    console.log(payload);
+    context.callbackWaitsForEmptyEventLoop = false;
+    getDataFromCowin().then(boolean => {
+        console.log(boolean);
+        callback(null, boolean);
+    }).catch(error => {
+        console.log(error);
+        callback(error.stack ? error.stack : error, false);
+    })
+}
+
+const getDataFromCowin = async () => {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const pre_parse = await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${process.env.DISTRICT_ID}&date=${day}-${month}-${year}`,{
+        headers: {
+            origin: "https://www.cowin.gov.in",
+            referer: "https://www.cowin.gov.in/",
+            "User-Agent": "PostmanRuntime/7.26.10",
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            "Cache-Control": "no-cache"
+        },
+        method: "GET"
+    });
+    if(pre_parse.status === 200){
+        const response = await pre_parse.json();
+        const centers = response.centers;
+        const Unavailable_Array = new Array();
+        for (let index = 0; index < centers.length; index++) {
+            const center = centers[index];
+            for (let j = 0; j < center.sessions.length; j++) {
+                const session = center.sessions[j];
+                if(session.min_age_limit === 18 && session.available_capacity > 0){
+                    await postToSlack("@channel \n ```"+JSON.stringify(center, null, 2)+"```");
+                } else {
+                    if(session.min_age_limit === 18){
+                        Unavailable_Array.push({
+                            center_name: center.name,
+                            center_pincode: center.pincode
+                        });
+                    }
+                }
+            }
+        }
+        console.table(Unavailable_Array);
+        return { data: true };
+    } else {
+        const status = pre_parse.status;
+        const response = await pre_parse.text();
+        return {
+            data: {
+                status,
+                response
+            }
+        }
+
+    }
+}
+
+const postToSlack = (data) => {
+    const url = `${process.env.SLACK_HOOK_URL}`;
+
+    const webhook = new IncomingWebhook(url);
+
+    const response_string = `${data}`;
+
+    return webhook.send({
+        text: response_string
+    });
+}
+
+// TESTING CODE
+// getDataFromCowin().then(boolean => {
+//     console.log(boolean);
+// }).catch(error => {
+//     console.log(error);
+// });
